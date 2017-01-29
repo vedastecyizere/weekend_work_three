62052935e8f2ef971bb9f3905a760546f46819b7
Vedaste Cyizere <revedaste@gmail.com>
2017-01-30 01:00:50 +0200

    answer on a question about tv and remote classes

--- oop/tv_and_remote.rb
+++ oop/tv_and_remote.rb
@@ -10,3 +10,47 @@
 # increment_volume (this will increase the tv's volume by 1)
 # decrement_volume (this will decrease the tv's volume by 1)
 # set_channel (this will change the tv's channel to whatever integer is passed to this method)
+
+class Remote 
+  attr_accessor :tv 
+
+  def toggle_power
+    if @power == "ON"
+      return "OFF"
+    else 
+      return "ON"
+    end 
+  end 
+
+  def increment_volume 
+    return @volume + 1 
+  end 
+  def decrement_volume
+    return @volume -= 1  
+  end 
+
+  def set_channel(number) 
+    return @channel << number 
+  end 
+
+class Tv < Remote  
+  attr_accessor :power, :volume, :channel
+
+  def initialize(setting)
+    @power = setting[:power]
+    @volume = setting[:volume]
+    @channel = setting[:channel]
+  end 
+end 
+tv = Tv.new(power: "ON", volume: 45, channel: 6)
+
+
+end
+remote = Remote.new 
+answer = tv.volume 
+puts answer
+
+answer = remote.increment_volume
+puts answer 
+answer = remote.set_channel(7)
+puts answer 
